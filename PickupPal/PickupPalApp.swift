e423008687f442011531b7b1633deda3821bcd8b
II-DW <dowon206@kakao.com>
2025-11-19 19:01:53 +0900

    Initial Commit

---
+++ PickupPal/PickupPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickupPalApp.swift
+//  PickupPal
+//
+//  Created by DOWON LEE on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct PickupPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PickupPalTests/PickupPalTests.swift
@@ -0,0 +1,17 @@
+//
+//  PickupPalTests.swift
+//  PickupPalTests
+//
+//  Created by DOWON LEE on 11/19/25.
+//
+
+import Testing
+@testable import PickupPal
+
+struct PickupPalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
